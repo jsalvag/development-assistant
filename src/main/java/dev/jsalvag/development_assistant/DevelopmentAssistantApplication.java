8f89737cadd8fc5b9c754796e5b948d3acb280ae
Jose Salvador Guevara Lunar <jose.guevara@globallogic.com>
2025-07-22 11:20:48 -0300

    first commit

---
+++ src/main/java/dev/jsalvag/development_assistant/DevelopmentAssistantApplication.java
@@ -0,0 +1,13 @@
+package dev.jsalvag.development_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jsalvag/development_assistant/DevelopmentAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jsalvag.development_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
